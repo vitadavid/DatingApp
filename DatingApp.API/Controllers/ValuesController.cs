e9283724fe035fdad3bb9e7799b755c981c40b31
David Víta <ext.david.vita@tieto.com>
2019-10-11 10:03:10 +0200

    End of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -18,6 +18,7 @@ namespace DatingApp.API.Controllers
             _context = context;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
